c6fcbd40d7403e2e8a9cd6a22482f986b1f9b717
Nathanael Billy <132752468+nathanbc29@users.noreply.github.com>
2024-12-16 23:44:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 
 # Streamlit App
-gradient_text_html = """
+st.markdown("""
 <style>
 .gradient-text {
     font-weight: bold;
@@ -46,7 +46,9 @@ gradient_text_html = """
 }
 </style>
 <div class="gradient-text">Predict Your Disease Based on Your Symptoms!</div>
-"""
+""", unsafe_allow_html=True)
+
+
 
 st.write("This project is a **Disease Prediction App** built with Streamlit and powered by machine learning algorithms. It predicts potential diseases based on user-inputted symptoms and patient profiles by analyzing a dataset of symptoms and diseases. The app uses a **Random Forest Classifier** to deliver predictions, and if the system determines that the confidence is too low, it suggests seeking a healthcare professional's opinion.")
 st.subheader("Features 🚀")
